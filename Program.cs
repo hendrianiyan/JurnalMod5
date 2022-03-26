1f754d2919d076eb1e2a63f0a7cf1873bcdc1b8b
hendrianiyan <hendrian582@gmail.com>
2022-03-26 11:43:50 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+
+namespace modul5_1302200104
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class sayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public sayaTubeVideo(string title)
+        {
+            Random random = new Random();
+            this.id = random.Next(0, 100000);
+            this.playCount = 0;
+
+            Debug.Assert(title.Length != 0, "title tidak boleh kosong");
+            Debug.Assert(title.Length < 100, "title terlalu panjang");
+        }
+        public void IncreaseplayCount(int angka)
+        {
+            playCount = playCount + angka;
+        }
+        public int Playcount => playCount;
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void printVideoDetails()
+        {
+            Console.WriteLine("title: " + title);
+            Console.WriteLine("id: " + id);
+            Console.WriteLine("playCount:" + playCount);
+        }
+    }
+    class SayaTubeUser
+    {
+        public int id;
+        public string username;
+
+    }
+}
